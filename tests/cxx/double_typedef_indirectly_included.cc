eeff04bfd6f77b93bc3b6ca65e653b30fc42f23b
george <george.georgiev@hotmail.com>
2016-05-09 21:04:14 -0700

    more than one typedef somehow is missed and including extra header is suggested

---
+++ tests/cxx/double_typedef_indirectly_included.cc
@@ -0,0 +1,33 @@
+//===--- implicit_ctor.cc - test input file for iwyu ----------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// If you define an API that accepts an argument of class type or
+// const reference to a class type with an implicit constructor, you
+// must provide the definition for the class.
+//
+// If you use an API and that use triggers an implicit constructor
+// call, you are *not* considered to have used that constructor,
+// relying on the previous rule to make the code complete definition
+// available.
+//
+// This tests that logic.
+
+#include "double_typedef_indirectly_included.h"
+#include "double_typedef_indirectly_included-i.h"
+#include <memory>
+
+void foo()
+{
+    ClassSPtr obj = std::make_shared<Class>();
+    SomethingElse something;
+}
+
+/**** IWYU_SUMMARY
+(tests/cxx/double_typedef_indirectly_included.cc has correct #includes/fwd-decls)
+***** IWYU_SUMMARY */
